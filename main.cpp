04683560eba00038ffea0469c22881526da8ff6d
Shubkush <48017414+Shubkush@users.noreply.github.com>
2019-10-29 23:32:47 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,52 @@
-#include<iostream>
-using namespace std ;
+#include <iostream>
+#include<algorithm>
+
+
+using namespace std;
 
 int main()
 {
-	int t, v ,s=-1 ;
-	long long int n, ans  ;
-	cin >>t ;
-	while(t--)
-	{
-		cin >> n ;
-		ans = (n-1)*9 + 19 ;
+    int n , i , j, cnt = 0 ;
+    cin >> n ;
+    unsigned long long int  vis[n] ;
+    bool b[n] ;
 
-		cout << ans << endl ;
-	}
-	return 0 ;
+    for(i = 0 ; i < n ; i++)
+        cin >> vis[i] ;
+
+    for(i = 0 ; i < n ; i++)
+        b[i] = true;
+
+    sort(vis,vis+n) ;
+    i = 0 ;
+    j = 1 ;
+        while(j != n )
+        {
+            if(vis[i]< vis[j] && (b[i] == b[j]) && (b[i] == true))
+            {
+                b[i] = false ;
+                b[j] = false ;
+                i++;
+                j++ ;
+            }
+            else if (vis[i]< vis[j] && (b[j] == false))
+            {
+                j++ ;
+            }
+            else
+            {
+                i++ ;
+                j++ ;
+            }
+        }
+    for(i =0 ; i<n ; i++)
+    {
+        if(b[i] == true)
+        {
+            cnt++ ;
+        }
+    }
+
+    cout << cnt << endl ;
+    return 0;
 }
